c4a61b6442d3d645bfdb387ab9f503a19e992586
kakwezi peace <peacekakwezi222@gmail.com>
2025-12-29 17:57:58 +0200

    feat(concurrent):  add SchedulerService class

--- src/main/java3/concurrent/SchedulerService.java
+++ src/main/java3/concurrent/SchedulerService.java
@@ -4,6 +4,7 @@ import java.util.concurrent.*;
 
 public class SchedulerService {
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
+// methods
 
     public void scheduleTask(Runnable task, long delay, long period, TimeUnit unit) {
         scheduler.scheduleAtFixedRate(task, delay, period, unit);
