bc9d8c4a5e8d3480bcbc414dce4705c7cfb88e27
kakwezi peace <peacekakwezi222@gmail.com>
2025-12-29 15:04:42 +0200

    feat(search): add SearchResult  class

--- src/main/java3/search/SearchResult.java
+++ src/main/java3/search/SearchResult.java
@@ -2,11 +2,12 @@ package search;
 
 import model.Student;
 import java.util.List;
-
+// adding record keyword
 public record SearchResult(List<Student> matches, long scanned, long timeMs) {
     // Extra constructor that takes int size instead of long scanned
     public SearchResult(List<Student> matches, int size, long timeMs)
     {
+
         this(matches, (long) size, timeMs);  // ✅ delegate to canonical constructor
     }
 }
